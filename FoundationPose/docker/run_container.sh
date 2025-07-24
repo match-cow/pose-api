9174e07f9bbbddf8ab406e9c8f784ff6f4a2281d
mengrui-code <meng-rui@outlook.com>
2025-07-24 14:39:44 +0200

    removed hard coded file path in run_container.sh and updated with rerelative file path

--- FoundationPose/docker/run_container.sh
+++ FoundationPose/docker/run_container.sh
@@ -1,3 +1,3 @@
 docker rm -f foundationpose
 DIR=$(pwd)/../
-xhost +  && docker run --gpus all --env NVIDIA_DISABLE_REQUIRE=1 -it --network=host --name foundationpose  --cap-add=SYS_PTRACE --security-opt seccomp=unconfined -v $DIR:$DIR -v /home:/home -v /mnt:/mnt -v /tmp/.X11-unix:/tmp/.X11-unix -v /tmp:/tmp  --ipc=host -e DISPLAY=${DISPLAY} -e GIT_INDEX_FILE foundationpose:latest bash -c "cd $DIR && export DIR=$(pwd) && nohup /opt/conda/envs/my/bin/python3 pose_api_server.py > pose_api.log 2>&1 & bash"
+docker run --gpus all --env NVIDIA_DISABLE_REQUIRE=1 -it --network=host --name foundationpose --cap-add=SYS_PTRACE --security-opt seccomp=unconfined -v $DIR:$DIR -v /home:/home -v /mnt:/mnt -v /tmp:/tmp --ipc=host foundationpose:latest bash -c "cd $DIR && export DIR=$(pwd) && nohup /opt/conda/envs/my/bin/python3 /home/meng/6d_pose_estimation_with_api/pose_api_server.py > pose_api.log 2>&1 & bash"
